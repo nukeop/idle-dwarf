1ff7789c66758a56fdd7c5ba885bc03caadec6a9
nukeop <alsw@protonmail.com>
2017-09-17 01:04:39 +0200

    redux setup

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './app.global.scss';
 
 import App from './App';
+import configureStore from './store/configureStore';
 
-ReactDOM.render(<App />, document.getElementById('react_container'));
+const store = configureStore();
+
+const render = Component => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Component />
+      </BrowserRouter>
+    </Provider>,
+    document.getElementById('react_container')
+  );
+}
+
+render(App);
---
+++ src/client/app/store/configureStore.js
@@ -0,0 +1,19 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import ReduxPromise from 'redux-promise';
+
+import rootReducer from '../reducers';
+
+export default function configureStore(initialState) {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  const store = createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(
+      applyMiddleware(ReduxPromise, thunk)
+    )
+  );
+
+  return store
+}
